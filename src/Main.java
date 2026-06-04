ada66b1e76350db81dcdcbe7fdeaa3819a222717
Wolfen <majchrzakwojciech06@gmail.com>
2026-06-04 13:34:25 +0200

    Init Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+void main(String[] args) {
+
+    Calculator c = new Calculator();
+
+    c.addition(2, 5);
+    c.substraction(5, 2);
+    c.muliplication(5, 7);
+    c.division(7, 2);
+
+    c.root(2, 4);
+
+    c.power(4, 2);
+
+    c.factorial(6);
+
+}
